adfff376a56701ea2bb5cdd24b6d8fbf83cf34c8
AquaRead <90779666+AquaRead@users.noreply.github.com>
2021-09-25 17:16:06 +0800

    Update homapage.dart

--- lib/screens/homapage.dart
+++ lib/screens/homapage.dart
@@ -1,7 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
-import 'package:my_aqua/screens/loginpage.dart';
-import 'package:my_aqua/screens/registerpage.dart';
+import 'package:my_aqua/screens/account.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,57 +11,163 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int currentTab = 0;
+
+  get child => null;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("AquaRead"),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Padding(
-            padding: EdgeInsets.all(20),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                SizedBox(
-                  height: 180,
-                  child: Image.asset('assets/2.png', fit: BoxFit.contain),
-                ),
-                Text(
-                  "Welcome to AquaRead",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  "Jetty",
-                  style: TextStyle(
-                    color: Colors.blue,
-                    fontWeight: FontWeight.w800,
+      body: Container(
+        padding: const EdgeInsets.only(top: 50),
+        child: Column(
+          children: [
+            Center(
+              child: SizedBox(
+                width: 350,
+                height: 200,
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  color: Colors.grey[400],
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10, bottom: 35),
+                        child: Column(
+                          children: [
+                            Center(
+                              child: Text(
+                                'Current Bills',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ),
+                            Center(
+                              child: Text(
+                                'August',
+                              ),
+                            ),
+                            Text(
+                              '10 cubic',
+                            ),
+                            Text(
+                              '₱ 1,200.00',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 20),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                Text(
-                  "Email",
-                  style: TextStyle(
-                    color: Colors.blue,
-                    fontWeight: FontWeight.w800,
-                  ),
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                ActionChip(
-                    label: Text("Log Out"),
-                    onPressed: () async {
-                      await FirebaseAuth.instance.signOut();
-                      setState(() {});
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => LoginPage()));
-                    }),
-              ],
+              ),
             ),
-          ),
-        ));
+            Container(
+              padding: const EdgeInsets.only(top: 50),
+              child: Column(
+                children: [
+                  Center(
+                    child: SizedBox(
+                      width: 450,
+                      height: 335,
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.only(
+                                topRight: Radius.circular(20.0),
+                                topLeft: Radius.circular(20.0))),
+                        color: Colors.grey,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                top: 10,
+                              ),
+                              child: Column(
+                                children: [
+                                  Center(
+                                    child: Text(
+                                      'Recent Transactions',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                  ),
+                                  Divider(
+                                    indent: 16,
+                                    endIndent: 16,
+                                    thickness: 3,
+                                    color: Colors.grey[400],
+                                  ),
+                                  Center(
+                                    child: Padding(
+                                      padding: const EdgeInsets.only(
+                                        top: 20.0,
+                                      ),
+                                      child: Row(
+                                        children: const <Widget>[
+                                          Expanded(
+                                            child: Text('August',
+                                                textAlign: TextAlign.center),
+                                          ),
+                                          Expanded(
+                                            child: Text('10 Cubic',
+                                                textAlign: TextAlign.center),
+                                          ),
+                                          Expanded(
+                                            child: Text('₱1,200.00'),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                  Divider(
+                                    indent: 16,
+                                    endIndent: 16,
+                                    thickness: 3,
+                                    color: Colors.grey[400],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          children: [
+            IconButton(
+                icon: Icon(Icons.home),
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => HomePage()));
+                }),
+            Spacer(),
+            IconButton(
+                icon: Icon(Icons.account_circle),
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => account()));
+                }),
+          ],
+        ),
+      ),
+      floatingActionButton:
+          FloatingActionButton(child: Icon(Icons.add), onPressed: () {}),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
   }
 }
